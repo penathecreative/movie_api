b0b6d6d0e1bee1b7f1987eba82c78fc9def45306
penathecreative <149909951+penathecreative@users.noreply.github.com>
2024-02-23 23:20:43 +0100

    Initial Mongo Committ

--- index.js
+++ index.js
@@ -4,113 +4,112 @@ const app = express();
 uuid = require("uuid");
 morgan = require("morgan");
 
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect("mongodb://localhost:27017/cfDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.use(bodyParser.json());
-
-let users = [
-  {
-    id: 1,
-    name: "Mike",
-    favouriteMovies: [],
-  },
-  {
-    id: 2,
-    name: "Tyson",
-    favouriteMovies: ["King of New York"],
-  },
-];
-
-let movies = [
-  {
-    Title: "Star Wars",
-    Description:
-      "A long time ago, in a galaxy far, far away, a young farm boy named Luke Skywalker embarks on an epic journey to rescue Princess Leia from the evil Galactic Empire.",
-    Director: {
-      Name: "George Lucas",
-      Bio: "George Walton Lucas Jr. is an American film director, producer, and screenwriter. He is best known for creating the Star Wars and Indiana Jones franchises.",
-      Birth: "May 14, 1944 (age 77 years), Modesto, California, United States",
-    },
-    Genre: {
-      Name: "Science Fiction",
-      Description:
-        "Science fiction (or sci-fi or SF) is a film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, mutants, interstellar travel, time travel, or other technologies.",
-    },
-  },
-  {
-    Title: "The Dark Knight",
-    Description:
-      "In Gotham City, the Joker, a criminal mastermind, unleashes chaos and sets out to undermine Batman's influence and create anarchy. Batman must confront the Joker's malevolent plans while dealing with his own internal struggles.",
-    Director: {
-      Name: "Christopher Nolan",
-      Bio: "Christopher Nolan is a British-American film director, producer, and screenwriter. He is known for directing complex and visually striking films such as Inception, Interstellar, and The Dark Knight Trilogy.",
-      Birth:
-        "July 30, 1970 (age 51 years), Westminster, London, United Kingdom",
-    },
-    Genre: {
-      Name: "Action, Crime, Drama",
-      Description:
-        "Action, Crime, Drama is a film genre that combines elements of action and crime films. It typically involves a protagonist who is trying to prevent or solve a crime, often with violence and thrilling sequences.",
-    },
-  },
-  {
-    Title: "King of New York",
-    Description:
-      "King of New York follows Frank White, a former drug lord recently released from prison, as he returns to the streets of New York with a desire to rebuild his empire. The film explores themes of crime, power, and morality.",
-    Director: {
-      Name: "Abel Ferrara",
-      Bio: "Abel Ferrara is an American filmmaker, known for his work in the crime and horror genres. In addition to directing King of New York, Ferrara has helmed films like Bad Lieutenant and The Funeral.",
-      Birth: "July 19, 1951 (age 70 years), The Bronx, New York, United States",
-    },
-    Genre: {
-      Name: "Crime, Thriller",
-      Description:
-        "Crime, Thriller is a film genre that combines elements of crime and thriller genres. It typically involves a protagonist who is involved in a crime or investigation, with suspenseful and thrilling elements.",
-    },
-  },
-];
 
 // Morgan middleware library to log all requests
 app.use(morgan("common"));
 
 //CREATE
-app.post("/users", (req, res) => {
-  const newUser = req.body;
+app.post("/users", async (req, res) => {
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists");
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
+});
 
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send("users need names");
-  }
+//READ: Get all users
+app.get("/users", async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+//READ: Get a user by username
+app.get("/users/:Username", async (req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 //UPDATE
-app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-
-  let user = users.find((user) => user.id == id);
-
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send("no such user");
-  }
+app.put("/users/:Username", async (req, res) => {
+  await Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }
+  ) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-//POST
-app.post("/users/:id/:movieTitle", (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find((user) => user.id == id);
-
-  if (user) {
-    user.favouriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-  } else {
-    res.status(400).send("no such user");
-  }
+// Add a movie to a user's list of favorites
+app.post("/users/:Username/movies/:MovieID", async (req, res) => {
+  await Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }
+  ) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 //DELETE
@@ -131,34 +130,45 @@ app.delete("/users/:id/:movieTitle", (req, res) => {
   }
 });
 
-//DELETE
-app.delete("/users/:id", (req, res) => {
-  const { id } = req.params;
-
-  let user = users.find((user) => user.id == id);
-
-  if (user) {
-    user = users.filter((user) => user.id != id);
-    res.status(200).send(`user ${id} has been deleted`);
-  } else {
-    res.status(400).send("no such user");
-  }
+// Delete a user by username
+app.delete("/users/:Username", async (req, res) => {
+  await Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + " was not found");
+      } else {
+        res.status(200).send(req.params.Username + " was deleted.");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-//READ
+//READ and return all movies at /movies
 app.get("/movies", (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-//READ
-app.get("/movies/:title", (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find((movie) => movie.Title === title);
+//READ: Return data about a single movie by title to the user
+app.get("/movies/:Title", async (req, res) => {
+  console.log("Title Parameter:", req.params.Title);
 
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send("no such movie");
+  try {
+    const movie = await Movie.findOne({ Title: req.params.Title });
+    console.log("Found Movie:", movie);
+    res.json(movie);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error: " + err.message);
   }
 });
 
