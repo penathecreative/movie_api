e7c8b1c5798eedf6ebc3c5d696a6288b1acd1cc3
penathecreative <149909951+penathecreative@users.noreply.github.com>
2024-02-27 20:00:41 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport"),
   LocalStrategy = require("passport-local").Strategy,
   Models = require("./models.js"),
-  passportJWT = require('passport-jwt');
+  passportJWT = require("passport-jwt");
 
 let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
