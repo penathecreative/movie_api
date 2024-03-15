8e642e51961acc12fba8c7ebda1b449afef6535d
penathecreative <149909951+penathecreative@users.noreply.github.com>
2024-03-16 00:30:31 +0100

    Passport re-enabled

--- index.js
+++ index.js
@@ -232,16 +232,20 @@ app.delete(
 );
 
 //READ and return all movies at /movies
-app.get("/movies", async (req, res) => {
-  Movies.find()
-    .then((movies) => {
-      res.status(201).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 //READ: Return data about a single movie by title to the user
 app.get(
